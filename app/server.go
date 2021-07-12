5e330eb2de902fb1d6a2862b48aaa2de210c51f2
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2021-07-12 13:25:20 -0300

    add healthcheck

--- app/server.go
+++ app/server.go
@@ -14,6 +14,9 @@ type server struct {
 func (s *server) start() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/metrics", promhttp.Handler().ServeHTTP)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 	addr := "0.0.0.0:" + s.config.port
 	log.Printf("Listening on %v", addr)
 	return http.ListenAndServe(addr, mux)
